34a8a042304a21e733e9cd5f4baa1dd4bfbd0b0b
jmmmel <youcantseeme.iamninja@gmail.com>
2015-02-14 22:45:53 -0700

    Create File and Load File are Working

--- app/src/main/java/edu/mel06002byui/multithreading/MainActivity.java
+++ app/src/main/java/edu/mel06002byui/multithreading/MainActivity.java
@@ -3,19 +3,29 @@ package edu.mel06002byui.multithreading;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String TAG = "MainActivity";
     private ArrayAdapter<Integer> evenAdapter;
     private ArrayAdapter<Integer> oddAdapter;
     @Override
@@ -48,17 +58,29 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void loadFromFile(View view) {
-        if(evenAdapter == null) {
-            
+        try{
+            InputStream readFiles = openFileInput("evens.txt");
+            if(readFiles != null) {
+                InputStreamReader inReader = new InputStreamReader(readFiles);
+                BufferedReader brInput = new BufferedReader(inReader);
+                String currLine = "";
+                StringBuilder evenInput = new StringBuilder();
+                while((currLine = brInput.readLine()) != null) {
+                    evenInput.append(currLine + " ");
+                }
+                readFiles.close();
+                TextView scriptureDisplayValue = new TextView(this);
+                scriptureDisplayValue.setTextSize(50);
+                scriptureDisplayValue.setText(evenInput.toString());
+                setContentView(scriptureDisplayValue);
+            }
         }
-        evenAdapter.clear();
-        oddAdapter.clear();
-
-        String evenFilename = "evens.txt";
-        String oddFilename = "odds.txt";
-        FileInputStream inputEven;
-        FileInputStream inputOdd;
-
+        catch (FileNotFoundException err){
+            Log.d(TAG, "FILE MISSING!!!!");
+        }
+        catch (IOException err) {
+            Log.d(TAG, "CAN'T WRITE!!!");
+        }
 
     }
 
@@ -69,21 +91,25 @@ public class MainActivity extends ActionBarActivity {
     public void createFile(View view) {
         String evenFilename = "evens.txt";
         String oddFilename = "odds.txt";
-        FileOutputStream outputStreamEven;
-        FileOutputStream outputStreamOdd;
+        OutputStreamWriter outputStreamEven;
+        OutputStreamWriter outputStreamOdd;
         try{
-            outputStreamEven = openFileOutput(evenFilename, Context.MODE_PRIVATE);
-            outputStreamOdd = openFileOutput(oddFilename, Context.MODE_PRIVATE);
+            outputStreamEven =
+                    new OutputStreamWriter(openFileOutput(evenFilename, Context.MODE_PRIVATE));
+            outputStreamOdd =
+                    new OutputStreamWriter(openFileOutput(oddFilename, Context.MODE_PRIVATE));
             for (Integer numInput = 1; numInput < 21; numInput++) {
                 if (numInput % 2 == 1) {
-                    outputStreamOdd.write(numInput.toString().getBytes());
+                    outputStreamOdd.write(numInput.toString() + "\n");
                 } else {
-                    outputStreamEven.write(numInput.toString().getBytes());
+                    outputStreamEven.write(numInput.toString() + "\n");
                 }
             }
+            outputStreamEven.close();
+            outputStreamOdd.close();
         }
         catch (Exception e) {
-            System.out.println("Error Creating Files");
+            Log.v(TAG, "FILE CREATION ISSUE!!!");
         }
     }
 }
